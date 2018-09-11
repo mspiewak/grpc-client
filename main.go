75b6eb0df0f96703638cc062fce7c972bbac4c27
Marcin Spiewak <marcin.spiewak@8x8.com>
2018-09-11 09:56:28 +0100

    initial data

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"time"
 
 	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
@@ -13,21 +14,31 @@ func main() {
 	serverAddr := "localhost:6565"
 
 	var wg sync.WaitGroup
-	wg.Add(10)
+	// wg.Add(10)
 
-	for i := 0; i < 10; i++ {
-		go send(serverAddr, &wg)
-	}
+	start := time.Now()
+	send(serverAddr, &wg)
 
-	wg.Wait()
+	t := time.Now()
+	elapsed := t.Sub(start)
+
+	fmt.Printf("Total time (connect + send): %v\n", elapsed)
+
+	// for i := 0; i < 10; i++ {
+	// 	go send(serverAddr, &wg)
+	// }
+
+	// wg.Wait()
 }
 
 func send(serverAddr string, wg *sync.WaitGroup) {
-	defer wg.Done()
+	// defer wg.Done()
 	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer conn.Close()
 
 	client := NewDetectorServiceClient(conn)
 	w, err := client.DetectAnswerMachine(context.Background())
@@ -37,16 +48,22 @@ func send(serverAddr string, wg *sync.WaitGroup) {
 
 	s := Signature{Data: "test"}
 
+	start := time.Now()
+	// for i := 0; i < 100; i++ {
 	if err := w.Send(&s); err != nil {
 		log.Fatal(err)
 	}
+	// }
 
-	res, err := w.CloseAndRecv()
+	_, err = w.CloseAndRecv()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(res)
+	t := time.Now()
+	elapsed := t.Sub(start)
 
-	defer conn.Close()
+	fmt.Printf("Send + response: %v\n", elapsed)
+
+	// fmt.Println(res.GetIsAnswerMachine())
 }
